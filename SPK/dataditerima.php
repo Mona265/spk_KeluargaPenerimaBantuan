3ceecffe2c17f100aecd85911ef57c0ca398576c
Mona265 <56064655+Mona265@users.noreply.github.com>
2020-01-01 23:58:20 +0700

    ubah layout data penerima

--- SPK/dataditerima.php
+++ SPK/dataditerima.php
@@ -1,36 +1,41 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Lihat data diterima</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
-    <title>Sistem pendukung Keputusan</title>
-  </head>
-  <body>
-    
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
             <!-- main css -->
         <link rel="stylesheet" href="css/style.css">
         <link rel="stylesheet" href="css/responsive.css">
  
-<table class="table">
-  <thead class="thead-dark" style="outline-color: blue">
-  <tr>
-      <td>
-        <a class="main_btn" href="halamanutama.php">Home</a>
+  
+</head>
+<body>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+      <a class="navbar-brand" href="halamanutama.php">Sistem Pendukung Keputusan Keluarga Penerima Bantuan</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-    </form><br>
-    </form> </td>
+      <div class="collapse navbar-collapse offset" id="navbarSupportedContent">
+              <ul class="nav navbar-nav menu_nav ml-auto">
+                <li class="nav-item"><a class="nav-link" href="halamanutama.php">Home</a></li> 
+                <li class="nav-item"><a class="nav-link" href="about-us.html">About</a></li> 
+            </div>
+    </div>
+  </nav>
+  
+
+<div class="container">
+    <h2>Daftar Penerima</h2>
+
+    <hr>
+    
+    <table class="table table-striped table-hover table-sm table-bordered">
+      <thead class="thead-dark">
+      
   </tr>
     <tr>
       <th scope="col" width="10%">No</th>
@@ -41,6 +46,7 @@
       
     </tr>
   </thead>
+  <tbody>
     <?php
     include 'koneksi.php';
     $nomer = 1;
@@ -61,7 +67,63 @@
         </tr>
         <?php
     }
-      ?>   
+      ?>
+      </tbody>   
 </table>
+
+ <footer class="footer_area">
+          <div class="container">
+            <div class="row footer_inner">
+              <div class="col-lg-5 col-sm-6">
+                <aside class="f_widget ab_widget">
+                  <div class="f_title">
+                    <h3>About Me</h3>
+                  </div>
+                  <p>Tugas Mata Kuliah Sistem Pendukung Keputusan</p>
+                            <p>Kelompok 5</p>
+                            <p>Anggiawan Prasetyo</p>
+                            <p>Dendy Dharmawan</p>
+                            <p>Musyaffa Nur Qisty</p>
+                            <p>Ramona Matovani</p>
+                  <p>Copyright 2019 Kelompok 5<i class="fa fa-heart-o" aria-hidden="true"></i></p>
+                </aside>
+              </div>
+              <div class="col-lg-5 col-sm-6">
+                <aside class="f_widget news_widget">
+                  <div class="f_title">
+                    <h3>Newsletter</h3>
+                  </div>
+                  <p>Stay updated with our latest trends</p>
+                  <div id="mc_embed_signup">
+                                <form target="_blank" action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01" method="get" class="subscribe_form relative">
+                                  <div class="input-group d-flex flex-row">
+                                        <input name="EMAIL" placeholder="Enter email address" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Email Address '" required="" type="email">
+                                        <button class="btn sub-btn"><span class="lnr lnr-arrow-right"></span></button>    
+                                    </div>        
+                                    <div class="mt-10 info"></div>
+                                </form>
+                            </div>
+                </aside>
+              </div>
+              <div class="col-lg-2">
+                <aside class="f_widget social_widget">
+                  <div class="f_title">
+                    <h3>Follow Me</h3>
+                  </div>
+                  <p>Let us be social</p>
+                  <ul class="list">
+                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                    <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+                    <li><a href="#"><i class="fa fa-behance"></i></a></li>
+                  </ul>
+                </aside>
+              </div>
+            </div>
+          </div>
+        </footer>
+ <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
  </body>
 </html>
